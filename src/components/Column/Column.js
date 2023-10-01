7a286fb2458639561aec27e2acb36408a9df124b
Robert Synowiec <rsynowiec81@gmail.com>
2023-10-01 17:52:36 +0200

    Add useMemo in Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,19 +3,23 @@ import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 import { useSelector } from 'react-redux';
 import { getFilteredCards } from '../../redux/store';
+import { useMemo } from 'react';
 
 const Column = props => {
     const cards = useSelector(state => getFilteredCards(state, props.id));
+
+    const renderedCards = useMemo(() => cards.map(card =>
+        <Card
+            key={card.id}
+            title={card.title}
+        />), [cards])
+
 
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
             <ul className={styles.cards}>
-                {cards.map(card =>
-                    <Card
-                        key={card.id}
-                        title={card.title}
-                    />)}
+                {renderedCards}
             </ul>
             <CardForm columnId={props.id} action={props.action} />
         </article>
