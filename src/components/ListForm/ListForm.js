0267062913f3c37b44f3914ed058d4dd80fb63e1
Robert Synowiec <rsynowiec81@gmail.com>
2023-10-05 22:12:39 +0200

    Add the function of adding lists

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,33 @@
+import styles from './ListForm.module.scss';
+import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
+import { addList } from '../../redux/store';
+
+const ListForm = props => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const { listId } = props;
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addList({ title, description, listId }));
+        setTitle('');
+        setDescription('');
+    };
+
+    return (
+        <form className={styles.listForm} onSubmit={handleSubmit}>
+            <label className={styles.label} >Title:</label> <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            <label className={styles.label}>Description:</label> <TextInput type="text" value={description} onChange={e => setDescription(e.target.value)} />
+            <Button>
+                Add list
+            </Button>
+        </form>
+    );
+};
+
+export default ListForm;
--- src/redux/store.js
+++ src/redux/store.js
@@ -21,6 +21,8 @@ export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 
+export const addList = payload => ({ type: 'ADD_LIST', payload });
+
 export const addSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 
 
@@ -30,6 +32,8 @@ const reducer = (state, action) => {
             return { ...state, columns: [...state.columns, { ...action.payload, id: shortid() }] };
         case 'ADD_CARD':
             return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }] };
+        case 'ADD_LIST':
+            return { ...state, lists: [...state.lists, { ...action.payload, id: shortid() }] };
         case 'UPDATE_SEARCHSTRING':
             return { ...state, searchString: action.payload };
 
