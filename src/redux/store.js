592ba22a50a29f6f9aecf2b71ee0ffe8589b2567
Robert Synowiec <rsynowiec81@gmail.com>
2023-10-05 17:39:17 +0200

    Add Lists component

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,6 +14,8 @@ export const getListById = ({ lists }, listId = 1) => lists.find(list => list.id
 export const getColumnsByList = ({ columns, listId }) => columns
     .filter(column => column.columnId === listId);
 
+export const getAllLists = (state => state.lists)
+
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
